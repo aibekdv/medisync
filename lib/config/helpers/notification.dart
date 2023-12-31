6e1897f2a461afc48aae82d08c841209f920f21f
Aibek Karataev <aibekdv@gmail.com>
2023-12-31 14:14:14 +0600

    removed some imports

--- lib/config/helpers/notification.dart
+++ lib/config/helpers/notification.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:medisync/feature/data/models/models.dart';
